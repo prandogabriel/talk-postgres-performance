a7d2a11765fb0add2ee92e042e271b2cbb1056d7
Gabriel Prando <gprando55@gmail.com>
2023-07-04 23:21:24 -0300

    feat: functions for bench

--- main.py
+++ main.py
@@ -1,71 +1,50 @@
-import psycopg2
 import timeit
 import matplotlib.pyplot as plt
+from setup import Setup
 
-# Função para criar tabela
-def create_table(cursor, table_name, columns, index=False):
-    columns_str = ', '.join([f'{col} text' for col in columns])
-    cursor.execute(f"CREATE TABLE {table_name} ({columns_str});")
-
-    if index:
-        for col in columns:
-            cursor.execute(f"CREATE INDEX idx_{col} ON {table_name} ({col});")
-
-# Função para executar a consulta
-def run_query(cursor, query):
-    cursor.execute(query)
-    conn.commit()
-
-# Criação da conexão com o banco
-conn = psycopg2.connect(
-    dbname="seu_banco_de_dados",
-    user="seu_usuario",
-    password="sua_senha",
-    host="localhost",
-    port="5432"
-)
-
-cur = conn.cursor()
+setup = Setup("localhost", "5432", "data", "postgres", "docker")
 
 sizes = [100, 1000, 10000, 100000, 1000000]
+
 no_index_times = []
 index_times = []
 
-for size in sizes:
-    # Criando e preenchendo a tabela
-    create_table(cur, 'tabela_teste', ['campo1'])
-    for i in range(size):
-        cur.execute(f"INSERT INTO tabela_teste(campo1) VALUES ('valor{i}');")
+setup.connect()
+setup.create_text_function()
 
-    conn.commit()
+for size in sizes:
+    setup.create_test_table(size)
 
     # Medindo tempo de execução sem índice
-    elapsed_time = timeit.timeit(lambda: run_query(cur, "EXPLAIN ANALYZE SELECT * FROM tabela_teste WHERE campo1 = 'valor500000';"), number=1)
+    elapsed_time = timeit.timeit(lambda: setup.execute_query("EXPLAIN ANALYZE SELECT * FROM test WHERE name = 'valor500000';"), number=1)
     no_index_times.append(elapsed_time)
 
-    # Dropando a tabela para criar com índice
-    cur.execute("DROP TABLE tabela_teste;")
+    setup.drop_index_idx_text()
 
-    # Criando a tabela com índice
-    create_table(cur, 'tabela_teste', ['campo1'], index=True)
-    for i in range(size):
-        cur.execute(f"INSERT INTO tabela_teste(campo1) VALUES ('valor{i}');")
-
-    conn.commit()
+    # Criando o índice
+    setup.create_index_idx_text()
 
     # Medindo tempo de execução com índice
-    elapsed_time = timeit.timeit(lambda: run_query(cur, "EXPLAIN ANALYZE SELECT * FROM tabela_teste WHERE campo1 = 'valor500000';"), number=1)
+    elapsed_time = timeit.timeit(lambda: setup.execute_query("EXPLAIN ANALYZE SELECT * FROM test WHERE name = 'valor500000';"), number=1)
     index_times.append(elapsed_time)
 
-    # Dropando a tabela para o próximo loop
-    cur.execute("DROP TABLE tabela_teste;")
+    setup.drop_index_idx_text()
+    setup.drop_table("test")
 
-conn.close()
+setup.close_connection()
+
+# Converter tempos para milissegundos
+no_index_times = [time * 1000 for time in no_index_times]
+index_times = [time * 1000 for time in index_times]
+
+print(no_index_times)
+print(index_times)
 
 # Plotando os resultados
 plt.plot(sizes, no_index_times, label='Sem índice')
 plt.plot(sizes, index_times, label='Com índice')
 plt.xlabel('Número de registros')
-plt.ylabel('Tempo de execução (segundos)')
+plt.ylabel('Tempo de execução (milissegundos)')
+plt.title('Tempo de execução da consulta por tamanho da tabela')
 plt.legend()
 plt.show()
---
+++ setup.py
@@ -0,0 +1,288 @@
+import psycopg2
+
+class Setup:
+    def __init__(self, host, port, database, user, password):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+        self.connection = None
+
+    def connect(self):
+        self.connection = psycopg2.connect(
+            host=self.host,
+            port=self.port,
+            database=self.database,
+            user=self.user,
+            password=self.password
+        )
+
+    def execute_query(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
+
+    def analyze_table(self, table_name):
+        analyze_query = f"ANALYZE {table_name};"
+        self.execute_query(analyze_query)
+
+    def table_exists(self, table_name):
+        check_table_query = f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}');"
+        cursor = self.connection.cursor()
+        cursor.execute(check_table_query)
+        result = cursor.fetchone()[0]
+        return result
+
+    def table_count_greater_equal(self, table_name, count):
+        check_count_query = f"SELECT COUNT(*) FROM {table_name};"
+        cursor = self.connection.cursor()
+        cursor.execute(check_count_query)
+        result = cursor.fetchone()[0]
+        return result >= count
+
+    def create_text_function(self):
+        text_function = """
+            CREATE OR REPLACE FUNCTION text(size integer) RETURNS text AS
+            $$
+            DECLARE
+                chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+                result text := '';
+                i integer := 0;
+            BEGIN
+                IF size < 0 THEN
+                    RAISE EXCEPTION 'Tamanho dado não pode ser menor que zero';
+                END IF;
+                FOR i IN 1..size LOOP
+                    result := result || chars[1 + random() * (array_length(chars, 1) - 1)];
+                END LOOP;
+                RETURN result;
+            END;
+            $$ LANGUAGE plpgsql;
+        """
+        self.execute_query(text_function)
+
+    def create_test_table(self, size):
+        if not self.table_exists("test"):
+            create_table_test = f"""
+                CREATE TABLE test(name varchar);
+                DO $$
+                BEGIN
+                    FOR i IN 1..{size} LOOP
+                        INSERT INTO test VALUES (text(10));
+                    END LOOP;
+                END;
+                $$ LANGUAGE plpgsql;
+            """
+            self.execute_query(create_table_test)
+        elif not self.table_count_greater_equal("test", size):
+            raise Exception("A tabela test já existe, mas não possui o número mínimo de registros.")
+
+        self.analyze_table("test")
+
+    def create_index_idx_text(self):
+        if not self.table_exists("test"):
+            raise Exception("A tabela test não existe.")
+
+        create_index = """
+            CREATE INDEX idx_text ON test(name);
+        """
+        self.execute_query(create_index)
+        self.analyze_table("test")
+
+    def drop_index_idx_text(self):
+        if not self.table_exists("test"):
+            raise Exception("A tabela test não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_text;
+        """
+        self.execute_query(drop_index)
+        self.analyze_table("test")
+
+    def create_index_idx_text_trgm(self):
+        if not self.table_exists("test"):
+            raise Exception("A tabela test não existe.")
+
+        create_extension = """
+            CREATE EXTENSION IF NOT EXISTS pg_trgm;
+        """
+        create_index = """
+            CREATE INDEX idx_text_trgm ON test USING GIN(name gin_trgm_ops);
+        """
+        self.execute_query(create_extension)
+        self.execute_query(create_index)
+        self.analyze_table("test")
+
+    def drop_index_idx_text_trgm(self):
+        if not self.table_exists("test"):
+            raise Exception("A tabela test não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_text_trgm;
+        """
+        self.execute_query(drop_index)
+
+    def create_person_table(self, size):
+        if not self.table_exists("person"):
+            create_table_person = f"""
+                CREATE TABLE person (
+                    id serial PRIMARY KEY,
+                    name varchar,
+                    gender varchar(1) CHECK (gender IN ('M','F'))
+                );
+                DO $$
+                BEGIN
+                    FOR i IN 1..{size} LOOP
+                        INSERT INTO person(name, gender) VALUES (text(10), 'M');
+                    END LOOP;
+                    FOR i IN 1..{size} LOOP
+                        INSERT INTO person(name, gender) VALUES (text(10), 'F');
+                    END LOOP;
+                END;
+                $$ LANGUAGE plpgsql;
+            """
+            self.execute_query(create_table_person)
+        elif not self.table_count_greater_equal("person", 2*size):
+            raise Exception("A tabela person já existe, mas não possui o número mínimo de registros.")
+
+        self.analyze_table("person")
+
+    def create_index_idx_gender_bitmap(self):
+        if not self.table_exists("person"):
+            raise Exception("A tabela person não existe.")
+
+        create_extension = """
+            CREATE EXTENSION IF NOT EXISTS btree_gin;
+        """
+        create_index = """
+            CREATE INDEX idx_gender_bitmap ON person USING gin (gender);
+        """
+        self.execute_query(create_extension)
+        self.execute_query(create_index)
+        self.analyze_table("person")
+
+    def drop_index_idx_gender_bitmap(self):
+        if not self.table_exists("person"):
+            raise Exception("A tabela person não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_gender_bitmap;
+        """
+        self.execute_query(drop_index)
+
+    def create_index_idx_name_btree(self):
+        if not self.table_exists("person"):
+            raise Exception("A tabela person não existe.")
+
+        create_index = """
+            CREATE INDEX idx_name_btree ON person USING btree (name);
+        """
+        self.execute_query(create_index)
+        self.analyze_table("person")
+
+    def drop_index_idx_name_btree(self):
+        if not self.table_exists("person"):
+            raise Exception("A tabela person não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_name_btree;
+        """
+        self.execute_query(drop_index)
+
+    def create_index_idx_name_hash(self):
+        if not self.table_exists("person"):
+            raise Exception("A tabela person não existe.")
+
+        create_index = """
+            CREATE INDEX idx_name_hash ON person USING hash (name);
+        """
+        self.execute_query(create_index)
+        self.analyze_table("person")
+
+    def drop_index_idx_name_hash(self):
+        if not self.table_exists("person"):
+            raise Exception("A tabela person não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_name_hash;
+        """
+        self.execute_query(drop_index)
+
+    def create_students_table(self, size):
+        if not self.table_exists("students"):
+            create_table_students = f"""
+                CREATE TABLE students (info jsonb);
+                DO $$
+                DECLARE vartype varchar[] := '{"quiz", "exam", "homework"}';
+                BEGIN
+                    FOR i IN 1..{size} LOOP
+                        FOR j IN 1..3 LOOP
+                            INSERT INTO students VALUES (
+                                (
+                                    '
+                                    {{
+                                        "student" : ' || i || ',
+                                        "type" : "' || vartype[j] || '",
+                                        "score" : ' || round(random() * 100) || '
+                                    }}
+                                    '
+                                )::json
+                            );
+                        END LOOP;
+                    END LOOP;
+                END;
+                $$ LANGUAGE plpgsql;
+            """
+            self.execute_query(create_table_students)
+        elif not self.table_count_greater_equal("students", 3*size):
+            raise Exception("A tabela students já existe, mas não possui o número mínimo de registros.")
+
+        self.analyze_table("students")
+
+    def create_index_idx_type(self):
+        if not self.table_exists("students"):
+            raise Exception("A tabela students não existe.")
+
+        create_index = """
+            CREATE INDEX idx_type ON students USING btree ((info->>'type'));
+        """
+        self.execute_query(create_index)
+        self.analyze_table("students")
+
+    def drop_index_idx_type(self):
+        if not self.table_exists("students"):
+            raise Exception("A tabela students não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_type;
+        """
+        self.execute_query(drop_index)
+        self.analyze_table("students")
+
+    def create_index_idx_json(self):
+        if not self.table_exists("students"):
+            raise Exception("A tabela students não existe.")
+
+        create_index = """
+            CREATE INDEX idx_json ON students USING GIN (info);
+        """
+        self.execute_query(create_index)
+        self.analyze_table("students")
+
+    def drop_index_idx_json(self):
+        if not self.table_exists("students"):
+            raise Exception("A tabela students não existe.")
+
+        drop_index = """
+            DROP INDEX IF EXISTS idx_json;
+        """
+        self.execute_query(drop_index)
+        self.analyze_table("students")
+        
+    def drop_table(self, table_name):
+        drop_table_query = f"DROP TABLE IF EXISTS {table_name};"
+        self.execute_query(drop_table_query)
+
+    def close_connection(self):
+        self.connection.close()
